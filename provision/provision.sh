186d3d59ccf68a68273a4625033db30a0c740aa6
Jon Brennecke <jpbrennecke@gmail.com>
2015-03-27 22:45:59 -0700

    fixing provision script

--- provision/provision.sh
+++ provision/provision.sh
@@ -15,26 +15,25 @@ cd /vagrant/www
 # Install aptitude repositories and update
 sudo apt-get -y update
 sudo apt-get -y install software-properties-common python-software-properties
-# sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-# sudo add-apt-repository 'deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main'
-# sudo add-apt-repository ppa:ondrej/php5-5.6
-# sudo apt-get -y update && sudo apt-get -y upgrade
-# sudo apt-get -y install git mariadb-server php5 php5-mysql php5-fpm nginx
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/ubuntu trusty main'
+sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install git mariadb-server php5 php5-mysql php5-fpm nginx
 
-# # Get the wordpress tarball and unpack it
-# wget http://wordpress.org/latest.tar.gz
-# tar xzvf latest.tar.gz
+# Get the wordpress tarball and unpack it
+wget http://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
 
 # Symlink our nginx conf file to /etc/nginx/, but save the old one in case we need it later
-# mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
-# ln -s /vagrant/site/private/nginx/nginx.conf /etc/nginx/nginx.conf
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+ln -s /vagrant/site/private/nginx/nginx.conf /etc/nginx/nginx.conf
 
-# # Create directory structure for nginx
-# mkdir -p /usr/share/nginx/logs
-# mkdir -p /usr/share/nginx/tmp/
+# Create directory structure for nginx
+mkdir -p /usr/share/nginx/logs
+mkdir -p /usr/share/nginx/tmp/
 
-# # load the mysql default tables
-# mysql -u root -p < /vagrant/site/private/sql/tables.sql
-
-# service nginx reload
+# Reload nginx now that we've updated the conf file
+service nginx reload
 
